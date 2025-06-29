cbf426e9e204f006ed39cd6f63474e92c1c14ba0
Inseok Lee <git@dlun.ch>
2025-06-29 17:56:23 +0900

    Unify target directory

--- build_examples.sh
+++ build_examples.sh
@@ -2,4 +2,4 @@
 
 cargo -Zunstable-options -C examples build --examples
 
-cargo run -p wipi-archiver -- examples/target/thumbv4t-none-eabi/debug/examples/helloworld Clet 00000000 PD000000 > helloworld.zip
+cargo run -p wipi-archiver -- target/thumbv4t-none-eabi/debug/examples/helloworld Clet 00000000 PD000000 > helloworld.zip
