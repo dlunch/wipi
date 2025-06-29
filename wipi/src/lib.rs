c0b2eb61ac7019c6d9342126ffcdf077a7b0d5f9
Inseok Lee <git@dlun.ch>
2025-06-29 19:13:04 +0900

    Make example build on host

--- wipi/src/lib.rs
+++ wipi/src/lib.rs
@@ -9,10 +9,17 @@ pub fn print(s: &str) {
     todo!()
 }
 
-#[cfg(target_os = "none")]
 #[panic_handler]
 #[allow(unused_variables)]
 fn panic_handler(info: &core::panic::PanicInfo) -> ! {
     // TODO
     loop {}
 }
+
+#[cfg(not(target_os = "none"))]
+#[unsafe(no_mangle)]
+pub extern "C" fn rust_eh_personality() {}
+
+#[cfg(target_os = "linux")]
+#[unsafe(export_name = "__libc_start_main")]
+pub fn main() {}
