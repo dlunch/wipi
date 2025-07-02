173cd9a090315eaff03912479a74987ee5605ffb
Inseok Lee <git@dlun.ch>
2025-07-02 19:56:08 +0900

    Use actual type only on target build

--- wipi_types/src/ktf.rs
+++ wipi_types/src/ktf.rs
@@ -1,11 +1,17 @@
+#[cfg(target_os = "none")]
 use core::ffi::c_char;
+
+#[cfg(target_os = "none")]
+type ExeInterfaceInitPtr = unsafe extern "C" fn(u32, u32, u32, u32, u32) -> u32;
+#[cfg(not(target_os = "none"))]
+type ExeInterfaceInitPtr = u32;
 
 #[repr(C)]
 #[derive(Clone, Copy)]
 pub struct ExeInterfaceFunctions {
     pub unk1: u32,
     pub unk2: u32,
-    pub fn_init: unsafe extern "C" fn(u32, u32, u32, u32, u32) -> u32,
+    pub fn_init: ExeInterfaceInitPtr,
     pub fn_get_default_dll: u32,
     pub fn_get_class: u32,
     pub fn_unk2: u32,
@@ -14,11 +20,21 @@ pub struct ExeInterfaceFunctions {
 
 unsafe impl Sync for ExeInterfaceFunctions {}
 
+#[cfg(target_os = "none")]
+type ExeInterfaceFunctionsPtr = *const ExeInterfaceFunctions;
+#[cfg(not(target_os = "none"))]
+type ExeInterfaceFunctionsPtr = u32;
+
+#[cfg(target_os = "none")]
+type ExeInterfaceNamePtr = *const c_char;
+#[cfg(not(target_os = "none"))]
+type ExeInterfaceNamePtr = u32;
+
 #[repr(C)]
 #[derive(Clone, Copy)]
 pub struct ExeInterface {
-    pub ptr_functions: *const ExeInterfaceFunctions,
-    pub ptr_name: *const c_char,
+    pub ptr_functions: ExeInterfaceFunctionsPtr,
+    pub ptr_name: ExeInterfaceNamePtr,
     pub unk1: u32,
     pub unk2: u32,
     pub unk3: u32,
@@ -29,15 +45,30 @@ pub struct ExeInterface {
 
 unsafe impl Sync for ExeInterface {}
 
+#[cfg(target_os = "none")]
+type ExeInterfacePtr = *const ExeInterface;
+#[cfg(not(target_os = "none"))]
+type ExeInterfacePtr = u32;
+
+#[cfg(target_os = "none")]
+type WipiExeNamePtr = *const c_char;
+#[cfg(not(target_os = "none"))]
+type WipiExeNamePtr = u32;
+
+#[cfg(target_os = "none")]
+type WipiExeInitPtr = unsafe extern "C" fn() -> u32;
+#[cfg(not(target_os = "none"))]
+type WipiExeInitPtr = u32;
+
 #[repr(C)]
 #[derive(Clone, Copy)]
 pub struct WipiExe {
-    pub ptr_exe_interface: *const ExeInterface,
-    pub ptr_name: *const c_char,
+    pub ptr_exe_interface: ExeInterfacePtr,
+    pub ptr_name: WipiExeNamePtr,
     pub unk1: u32,
     pub unk2: u32,
     pub fn_unk1: u32,
-    pub fn_init: unsafe extern "C" fn() -> u32,
+    pub fn_init: WipiExeInitPtr,
     pub unk3: u32,
     pub unk4: u32,
     pub fn_unk3: u32,
