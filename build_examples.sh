c27ba3c7b44fe7104636d28842c6d18e77b5aced
Inseok Lee <git@dlun.ch>
2025-07-02 16:33:25 +0900

    Use consistent underscore

--- build_examples.sh
+++ build_examples.sh
@@ -2,4 +2,4 @@
 
 cargo -Zbuild-std=core build -p examples --examples --target thumbv4t-none-eabi
 
-cargo run -p wipi-archiver -- target/thumbv4t-none-eabi/debug/examples/helloworld Clet 00000000 PD000000 > target/helloworld.zip
+cargo run -p wipi_archiver -- target/thumbv4t-none-eabi/debug/examples/helloworld Clet 00000000 PD000000 > target/helloworld.zip
