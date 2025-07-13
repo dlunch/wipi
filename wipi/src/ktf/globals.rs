f1e12301b84389987cc87b6f19307b9de8b007d2
Inseok Lee <git@dlun.ch>
2025-07-13 09:40:00 +0900

    Use correc ttype for knlinterface

--- wipi/src/ktf/globals.rs
+++ wipi/src/ktf/globals.rs
@@ -2,6 +2,7 @@ use core::ptr::null;
 
 use wipi_types::ktf::{
     InitParam0, InitParam1, InitParam2, InitParam3, InitParam4, java::WIPIJBInterface,
+    kernel::WIPICKnlInterface,
 };
 
 pub(super) static mut INIT_PARAM_0: *const InitParam0 = null();
@@ -9,5 +10,5 @@ pub(super) static mut INIT_PARAM_1: *const InitParam1 = null();
 pub(super) static mut INIT_PARAM_2: *const InitParam2 = null();
 pub(super) static mut INIT_PARAM_3: *const InitParam3 = null();
 pub(super) static mut INIT_PARAM_4: *const InitParam4 = null();
-pub(super) static mut WIPIC_KNLINTERFACE: *const () = null();
+pub(super) static mut WIPIC_KNLINTERFACE: *const WIPICKnlInterface = null();
 pub(super) static mut WIPI_JBINTERFACE: *const WIPIJBInterface = null();
