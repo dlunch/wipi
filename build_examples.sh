d90f1b51e889ade89062ae6bba2ebac5ecaab453
Inseok Lee <git@dlun.ch>
2025-07-13 20:33:12 +0900

    Add lgt feature

--- build_examples.sh
+++ build_examples.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-cargo -Zbuild-std=core build -p examples --examples --target thumbv4t-none-eabi
+cargo -Zbuild-std=core build -p examples --target thumbv4t-none-eabi --features ktf
 
-cargo run -p wipi_archiver -- target/thumbv4t-none-eabi/debug/examples/helloworld Clet 00000000 PD000000 > target/helloworld.zip
+cargo run -p wipi_archiver -- target/thumbv4t-none-eabi/debug/helloworld Clet 00000000 PD000000 > target/helloworld.zip
