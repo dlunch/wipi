8e17d137a213fa9c7628a99f91f5834b4eca667d
Inseok Lee <git@dlun.ch>
2025-07-19 10:30:11 +0900

    Disable debug info on example

--- build_examples.sh
+++ build_examples.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-cargo -Zbuild-std=core build -p examples --target thumbv4t-none-eabi --features ktf
+cargo -Zbuild-std=core build -p examples --target thumbv4t-none-eabi --features ktf --profile examples
 
-cargo run -p wipi_archiver -- ktf target/thumbv4t-none-eabi/debug/helloworld Clet 00000000 PD000000 > target/helloworld_ktf.zip
+cargo run -p wipi_archiver -- ktf target/thumbv4t-none-eabi/examples/helloworld Clet 00000000 PD000000 > target/helloworld_ktf.zip
 
-cargo -Zbuild-std=core build -p examples --target thumbv4t-none-eabi --features lgt
+cargo -Zbuild-std=core build -p examples --target thumbv4t-none-eabi --features lgt --profile examples
 
-cargo run -p wipi_archiver -- lgt target/thumbv4t-none-eabi/debug/helloworld Clet 00000000 PD000000 > target/helloworld_lgt.zip
+cargo run -p wipi_archiver -- lgt target/thumbv4t-none-eabi/examples/helloworld Clet 00000000 PD000000 > target/helloworld_lgt.zip
