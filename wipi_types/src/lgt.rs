184dff05f6cf75296f2c456f41936f0cb791af57
Inseok Lee <git@dlun.ch>
2025-07-20 12:03:30 +0900

    Pub field

--- wipi_types/src/lgt.rs
+++ wipi_types/src/lgt.rs
@@ -3,24 +3,24 @@ use bytemuck::{Pod, Zeroable};
 #[repr(C)]
 #[derive(Clone, Copy, Pod, Zeroable)]
 pub struct InitStruct {
-    unk1: u32,
-    fn_init: u32,
-    ptr_str_init: u32, // pointer to string "init"
+    pub unk1: u32,
+    pub fn_init: u32,
+    pub ptr_str_init: u32, // pointer to string "init"
 }
 
 #[repr(C)]
 #[derive(Clone, Copy, Pod, Zeroable)]
 pub struct InitParam1 {
-    unk1: [u8; 512],
-    unk2: [u8; 20],
-    ptr_init_struct: u32,
+    pub unk1: [u8; 512],
+    pub unk2: [u8; 20],
+    pub ptr_init_struct: u32,
 }
 
 #[repr(C)]
 #[derive(Clone, Copy, Pod, Zeroable)]
 pub struct InitParam2 {
-    fn_get_import_table: u32,
-    fn_get_import_function: u32,
-    fn_unk3: u32,
-    fn_unk4: u32,
+    pub fn_get_import_table: u32,
+    pub fn_get_import_function: u32,
+    pub fn_unk3: u32,
+    pub fn_unk4: u32,
 }
