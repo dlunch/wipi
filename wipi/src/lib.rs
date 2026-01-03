12cd7f580ba5874a60ac86560ebc5247f90d83af
Inseok Lee <git@dlun.ch>
2026-01-03 10:17:35 +0900

    Move some codes

--- wipi/src/lib.rs
+++ wipi/src/lib.rs
@@ -7,6 +7,7 @@ pub mod app;
 pub mod console;
 pub mod framebuffer;
 mod panic_handler;
+mod sys;
 
 #[macro_export]
 macro_rules! println {
@@ -16,46 +17,6 @@ macro_rules! println {
         let _ = write!($crate::console::Console, "\n");
     }};
 }
-
-use alloc::boxed::Box;
-
-use crate::app::App;
-
-unsafe extern "Rust" {
-    fn __wipi_main() -> Box<dyn App>;
-}
-
-static mut APP: Option<Box<dyn App>> = None;
-
-#[unsafe(export_name = "startClet")]
-extern "C" fn start_clet() {
-    let app = unsafe { __wipi_main() };
-    unsafe {
-        APP = Some(app);
-    }
-}
-
-#[unsafe(export_name = "destroyClet")]
-extern "C" fn destroy_clet() {}
-
-#[unsafe(export_name = "paintClet")]
-#[allow(static_mut_refs)]
-extern "C" fn paint_clet() {
-    unsafe {
-        if let Some(app) = APP.as_mut() {
-            app.on_paint();
-        }
-    }
-}
-
-#[unsafe(export_name = "pauseClet")]
-extern "C" fn pause_clet() {}
-
-#[unsafe(export_name = "resumeClet")]
-extern "C" fn resume_clet() {}
-
-#[unsafe(export_name = "handleCletEvent")]
-extern "C" fn handle_clet_event() {}
 
 // re-exports
 pub mod kernel {
