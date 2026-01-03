0ff2c132a93b9b5ac11fc1b1d2d0b18b44394e2c
Inseok Lee <git@dlun.ch>
2026-01-03 10:17:02 +0900

    Move some codes

--- wipi/src/app.rs
+++ wipi/src/app.rs
@@ -1,48 +1,6 @@
-use alloc::boxed::Box;
-
-static mut APP: Option<Box<dyn App>> = None;
-
 pub trait App {
     fn on_paint(&mut self);
     fn on_pause(&mut self);
     fn on_resume(&mut self);
     fn on_event(&mut self);
 }
-
-fn register_app(app: Box<dyn App>) {
-    unsafe {
-        APP = Some(app);
-    }
-}
-
-unsafe extern "Rust" {
-    fn __wipi_main() -> Box<dyn App>;
-}
-
-#[unsafe(export_name = "startClet")]
-extern "C" fn start_clet() {
-    let app = unsafe { __wipi_main() };
-    register_app(app);
-}
-
-#[unsafe(export_name = "destroyClet")]
-extern "C" fn destroy_clet() {}
-
-#[unsafe(export_name = "paintClet")]
-#[allow(static_mut_refs)]
-extern "C" fn paint_clet() {
-    unsafe {
-        if let Some(app) = APP.as_mut() {
-            app.on_paint();
-        }
-    }
-}
-
-#[unsafe(export_name = "pauseClet")]
-extern "C" fn pause_clet() {}
-
-#[unsafe(export_name = "resumeClet")]
-extern "C" fn resume_clet() {}
-
-#[unsafe(export_name = "handleCletEvent")]
-extern "C" fn handle_clet_event() {}
--- wipi/src/lib.rs
+++ wipi/src/lib.rs
@@ -17,6 +17,46 @@ macro_rules! println {
     }};
 }
 
+use alloc::boxed::Box;
+
+use crate::app::App;
+
+unsafe extern "Rust" {
+    fn __wipi_main() -> Box<dyn App>;
+}
+
+static mut APP: Option<Box<dyn App>> = None;
+
+#[unsafe(export_name = "startClet")]
+extern "C" fn start_clet() {
+    let app = unsafe { __wipi_main() };
+    unsafe {
+        APP = Some(app);
+    }
+}
+
+#[unsafe(export_name = "destroyClet")]
+extern "C" fn destroy_clet() {}
+
+#[unsafe(export_name = "paintClet")]
+#[allow(static_mut_refs)]
+extern "C" fn paint_clet() {
+    unsafe {
+        if let Some(app) = APP.as_mut() {
+            app.on_paint();
+        }
+    }
+}
+
+#[unsafe(export_name = "pauseClet")]
+extern "C" fn pause_clet() {}
+
+#[unsafe(export_name = "resumeClet")]
+extern "C" fn resume_clet() {}
+
+#[unsafe(export_name = "handleCletEvent")]
+extern "C" fn handle_clet_event() {}
+
 // re-exports
 pub mod kernel {
     pub use wipic_sys::kernel::exit;
