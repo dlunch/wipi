942f5fda33b23246cecfa559ae1aab714f835c53
Inseok Lee <git@dlun.ch>
2026-01-11 20:40:56 +0900

    Move actual logic into simulation crate

--- host_crates/wipic_simulation/src/lib.rs
+++ host_crates/wipic_simulation/src/lib.rs
@@ -1,2 +1,14 @@
 pub mod graphics;
 pub mod kernel;
+
+use std::{thread, time::Duration};
+
+pub fn simulation_start(start_clet: unsafe extern "C" fn(), paint_clet: unsafe extern "C" fn()) {
+    unsafe { start_clet() };
+
+    // TODO event loop
+    loop {
+        unsafe { paint_clet() };
+        thread::sleep(Duration::from_millis(16));
+    }
+}
--- wipi_boot/src/simulation.rs
+++ wipi_boot/src/simulation.rs
@@ -1,15 +1,5 @@
-extern crate std;
-
-use std::{thread, time::Duration};
-
 use crate::{paint_clet, start_clet};
 
 pub fn simulation_start() {
-    unsafe { start_clet() };
-
-    // TODO event loop
-    loop {
-        unsafe { paint_clet() };
-        thread::sleep(Duration::from_millis(16));
-    }
+    wipic_simulation::simulation_start(start_clet, paint_clet);
 }
